c98c48a0305ffa3ca43c8f719ffc1a7badf907e0
GetLocalPlayer <GetLocalPlayer@gmail.com>
2022-08-08 14:52:01 +0400

    Added pause behaviour to "menu" scene on Esc just pressed

--- Menu.gd
+++ Menu.gd
@@ -8,3 +8,15 @@ func _on_QuitGame_pressed():
 
 func _on_Retry_pressed():
 	get_tree().reload_current_scene()
+
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_cancel"):
+		if not visible:
+			$Retry.hide()
+			show()
+			get_tree().paused = true
+		elif get_tree().paused:
+			$Retry.show()
+			hide()
+			get_tree().paused = false
