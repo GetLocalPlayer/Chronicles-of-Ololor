773c22098685bba8f2047c8af73d7489f94cb1f6
GetLocalPlayer <GetLocalPlayer@gmail.com>
2022-08-28 12:59:35 +0400

    Moving to Godot 3.5 with required changes

--- Models/Characters/Emma/LowHPScreen.gd
+++ Models/Characters/Emma/LowHPScreen.gd
@@ -8,6 +8,6 @@ export (float, 1, 100, 1) var hp_threshold = 70
 
 func _process(_delta):
 	var hp = owner.health / owner.max_health
-	var threshold = hp_threshold / 100
+	var threshold = hp_threshold / 100.0
 	visible = hp < threshold
 	modulate.a = 1 - hp / threshold
